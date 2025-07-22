42f4596226f33d13179e38467dcb68cbc6750fec
rino00o <juanfermora1807@gmail.com>
2025-07-22 16:24:15 -0500

    Update Carta.cpp

--- Carta.cpp
+++ Carta.cpp
@@ -1,23 +1,26 @@
-// Implementación de la clase Carta
-
+/*
+Implementación de la clase Carta.h
+*/
 #include "Carta.h"
 
-// Constructor: inicializa el nombre y asigna el valor según las reglas del Blackjack.
+// Constructor: inicializa el valor y el nombre de la carta
 Carta::Carta(char nombre) : nombre(nombre) {
-    // Si la carta es As, su valor es 11 inicialmente (puede ser 1, según la mano)
-    if (nombre == 'A') valor = 11;
-    // Si la carta es figura, su valor es 10
-    else if (nombre == 'T' || nombre == 'J' || nombre == 'Q' || nombre == 'K') valor = 10;
-    // Si la carta es número entre 2 y 9, su valor es el número
-    else valor = nombre - '0';
+    if (nombre == 'A') {
+        valor = 11; // As vale 11 por defecto
+    }
+    else if (nombre == 'T' || nombre == 'J' || nombre == 'Q' || nombre == 'K') {
+        valor = 10; // Figuras valen 10
+    }
+    else {
+        valor = nombre - '0'; // Convierte char a int
+    }
 }
-
-// Devuelve el valor nominal de la carta
+//Devuelve el valor de la carta
 int Carta::obtenerValor() const {
     return valor;
 }
-
-// Devuelve el nombre de la carta
+//Devuelve el noombre de la carta 
 char Carta::obtenerNombre() const {
     return nombre;
 }
+
